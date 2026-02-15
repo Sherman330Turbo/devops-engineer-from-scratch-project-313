f5d2272b549d34a88b62a124006e7051b09c01a8
Oleg Sherman <voodooleg@gmail.com>
2026-02-15 10:00:47 +0300

    Step-4: добавил в модель created_at

--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 
 from sqlmodel import Field, SQLModel
@@ -8,6 +9,9 @@ class Link(SQLModel, table=True):
     original_url: str
     short_name: str
     short_url: str
+    created_at: Optional[datetime] = Field(
+        default_factory=lambda: datetime.now(timezone.utc)
+    )
 
 
 class LinkUpdate(SQLModel):
