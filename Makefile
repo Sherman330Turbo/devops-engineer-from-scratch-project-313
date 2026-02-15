c38d924a17c3bfbe633de7c5dc8ea5566f3bb681
Oleg Sherman <voodooleg@gmail.com>
2026-02-15 16:23:34 +0300

    step-6: Добавил фронтовый проект, запускающийся локально одновременно с бекендом

--- Makefile
+++ Makefile
@@ -1,5 +1,26 @@
+FRAMEWORK ?= react
+API_URL ?= http://127.0.0.1:8080
+
+.PHONY: start start-backend start-frontend install lint test
+
 start:
+	npx concurrently \
+		--kill-others-on-fail \
+		--names "back,front" \
+		--prefix-colors "magenta,cyan" \
+		"$(MAKE) start-backend" \
+		"$(MAKE) start-frontend FRAMEWORK=$(FRAMEWORK)"
+
+start-backend:
 	uv run flask --app src/main --debug run --port 8080
+
+start-frontend:
+	@if [ "$(FRAMEWORK)" = "react" ]; then \
+		API_URL="$(API_URL)" npx start-hexlet-devops-deploy-crud-frontend; \
+	else \
+		echo "No framework this time"; \
+		exit 0; \
+	fi
 
 install:
 	python3 -m pip install -e ".[dev]"
--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
+import os
+
 from flask import Flask
+from flask_cors import CORS
 from sqlmodel import SQLModel
 
 from .db import engine
@@ -8,6 +11,21 @@ from .routes import register_routes
 
 def create_app() -> Flask:
     app = Flask(__name__)
+
+    if os.getenv("IS_DEVELOPMENT", "False") == "True":
+        CORS(
+            app,
+            resources={
+                r"/api/*": {
+                    "origins": [
+                        "http://127.0.0.1:5173",
+                    ]
+                }
+            },
+            supports_credentials=True,
+            methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            expose_headers=["Content-Range"],
+        )
 
     SQLModel.metadata.create_all(engine)
 
